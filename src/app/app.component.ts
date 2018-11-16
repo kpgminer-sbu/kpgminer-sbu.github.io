dd24017bf4c0f4b08ea927612eaddf2dc5db5594
yagrawala <eternalyogi@Yogeshs-MacBook-Pro.local>
2018-11-16 18:28:07 -0500

    initial commit of boilerplate app

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {SelectModule} from 'ng2-select';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent{
+  title = 'KPG Miner';
+}
